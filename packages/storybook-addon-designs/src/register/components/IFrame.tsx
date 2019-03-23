66f899afa8eef1638cc76560f759d554a719de93
pocka <pockawoooh@gmail.com>
2019-03-23 18:41:55 +0900

    [feat] Add loading indicator
    * Add loading indicator for iframe (and Figma).

--- packages/storybook-addon-designs/src/register/components/IFrame.tsx
+++ packages/storybook-addon-designs/src/register/components/IFrame.tsx
@@ -2,6 +2,8 @@
 import { SFC } from 'react'
 import { css, jsx } from '@storybook/theming'
 
+import { Placeholder } from '@storybook/components'
+
 import { IFrameConfigBase } from '../../config'
 
 interface Props {
@@ -9,18 +11,41 @@ interface Props {
 }
 
 export const IFrame: SFC<Props> = ({ config }) => (
-  <iframe
-    css={$iframe}
-    src={config.url}
-    allowFullScreen={config.allowFullscreen}
-  />
+  <div css={$container}>
+    <Placeholder css={$loading}>Loading...</Placeholder>
+    <iframe
+      css={$iframe}
+      src={config.url}
+      allowFullScreen={config.allowFullscreen}
+    />
+  </div>
 )
 
 export default IFrame
 
-// NOTE: height:99% is to prevent panel scrolling
+const $container = css`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  overflow: hidden;
+`
+
+const $loading = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+  transform: translate(-50%, -50%);
+`
+
 const $iframe = css`
+  position: relative;
   width: 100%;
-  height: 99%;
+  height: 100%;
   border: none;
+
+  z-index: 1;
 `
