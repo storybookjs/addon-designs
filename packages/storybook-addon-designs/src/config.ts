ec640c7113bd69a3eacb65823e0287a3b23c1ea2
pocka <pockawoooh@gmail.com>
2019-03-24 19:37:17 +0900

    style: Add comments for interfaces

--- packages/storybook-addon-designs/src/config.ts
+++ packages/storybook-addon-designs/src/config.ts
@@ -4,26 +4,62 @@ export type Config = IFrameConfig | FigmaConfig | PdfConfig
  * Options for rendering iframe.
  */
 export interface IFrameConfigBase {
+  /**
+   * An URL to show.
+   */
   url: string
+
+  /**
+   * Allow embeded site to go in fullscreen mode.
+   */
   allowFullscreen?: boolean
 }
 
+/**
+ * Render iframe.
+ */
 export interface IFrameConfig extends IFrameConfigBase {
   type: 'iframe'
 }
 
+/**
+ * Render Figma Live Preview.
+ */
 export interface FigmaConfig extends IFrameConfigBase {
   type: 'figma'
 
+  /**
+   * A string identifies your site.
+   */
   embedHost?: string
 }
 
+/**
+ * Render PDF file.
+ */
 export interface PdfConfig {
   type: 'pdf'
 
+  /**
+   * An URL for the PDF file.
+   */
   url: string
 
+  /**
+   * Default scale value.
+   * Must be greater than 0.
+   */
   scale?: number
+
+  /**
+   * Default offset value.
+   * Incresing X value moves document right.
+   * Incresing Y value moves document down.
+   */
   offset?: [number, number]
+
+  /**
+   * Default page number to show.
+   */
   page?: number
 }
