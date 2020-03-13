716485c32b2096b7d38ff1910453671ebc464181
Shota Fuji <pockawoooh@gmail.com>
2020-03-13 19:28:31 +0000

    misc: New logo
    Renewed the logo because existing ones were horrible!
    
    Co-authored-by: ayapagos <kuda@impv.co.jp>

--- packages/examples/.storybook/manager.js
+++ packages/examples/.storybook/manager.js
@@ -1,7 +1,7 @@
 import { addons } from '@storybook/addons'
 import { create } from '@storybook/theming'
 
-import logo from '@storybook-addon-designs/assets/logo-with-text.png'
+import logo from '@storybook-addon-designs/assets/logo-with-text.svg'
 import pkg from 'storybook-addon-designs/package.json'
 
 addons.setConfig({
