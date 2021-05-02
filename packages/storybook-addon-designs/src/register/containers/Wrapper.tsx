15cf160d80ccbf310f183f6a45be141d784c68e5
Michael Shilman <michael@lab80.co>
2021-05-02 21:32:06 +0800

    Simplify wrapper with storybook hooks API

--- packages/storybook-addon-designs/src/register/containers/Wrapper.tsx
+++ packages/storybook-addon-designs/src/register/containers/Wrapper.tsx
@@ -1,51 +1,25 @@
 /** @jsx jsx */
-import { Fragment, SFC, useEffect, useState } from 'react'
+import { FC } from 'react'
 import { jsx } from '@storybook/theming'
-import addons from '@storybook/addons'
-import { STORY_CHANGED } from '@storybook/core-events'
-
-import { Link, Placeholder, TabsState } from '@storybook/components'
+import { useParameter, useStorybookState } from '@storybook/api'
 
 import { Config } from '../../config'
-import { Events, ParameterName } from '../../addon'
+import { ParameterName } from '../../addon'
 
 import { Wrapper as Pure } from '../components/Wrapper'
 
 interface Props {
-  channel: ReturnType<typeof addons['getChannel']>
-
-  api: any
-
   active: boolean
 }
 
-export const Wrapper: SFC<Props> = ({ active, api, channel }) => {
-  const [config, setConfig] = useState<Config | Config[]>()
-  const [storyId, changeStory] = useState<string>()
-
-  useEffect(() => {
-    const onStoryChanged = (id: string) => {
-      changeStory(id)
-
-      const cfg = api.getParameters(id, ParameterName)
-
-      setConfig(prev => (cfg !== prev ? cfg : prev))
-    }
-
-    channel.on(Events.UpdateConfig, setConfig)
-    channel.on(STORY_CHANGED, onStoryChanged)
-
-    return () => {
-      channel.removeListener(Events.UpdateConfig, setConfig)
-      channel.removeListener(STORY_CHANGED, onStoryChanged)
-    }
-  }, [])
-
+export const Wrapper: FC<Props> = ({ active }) => {
   if (!active) {
     return null
   }
 
-  return <Pure key={storyId} config={config} />
+  const state = useStorybookState()
+  const config = useParameter(ParameterName) as Config
+  return <Pure key={state.storyId} config={config} />
 }
 
 export default Wrapper
