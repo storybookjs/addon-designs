4bf18ec61ff58ac87e700423f41042e36d4de049
Valentin Palkovic <valentin@chromatic.com>
2026-02-06 15:51:05 +0100

    Feature: Add CSF Factory definePreview parameter types

--- packages/storybook-addon-designs/src/index.ts
+++ packages/storybook-addon-designs/src/index.ts
@@ -1,5 +1,8 @@
+import { definePreviewAddon } from "storybook/internal/csf";
+
 import { PanelName, ParameterName } from "./constants";
 import { Config } from "./config";
+import type { DesignTypes } from "./types";
 
 /**
  * Dumb function to ensure typings or enchance IDE auto completion.
@@ -10,3 +13,6 @@ export const config = (c: Config | Config[]) => c;
  * Useful exports for third party developers and advanced usages
  */
 export { Config, ParameterName as PARAM_KEY, PanelName };
+export type { DesignTypes } from "./types";
+
+export default () => definePreviewAddon<DesignTypes>({});
